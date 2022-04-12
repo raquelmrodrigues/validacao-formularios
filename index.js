ae1830871efd36021d67a3fd16292888a8e9dac7
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2022-04-12 12:59:15 -0300

    validação front

--- index.js
+++ index.js
@@ -26,6 +26,26 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
+app.post("/form", (req, res) => {
+    var {email, nome, pontos} = req.body;
+    var emailError;
+    var nomeError;
+
+    if(email == undefined || email == "") {
+        emailError = "E-mail não pode ser vazio";
+    }
+
+    if(nome == undefined || nome == "") {
+        nomeError = "Nome não pode ser vazio";
+    }
+
+    if(emailError != undefined || nomeError != undefined) {
+        res.redirect("/");
+    } else {
+        res.send("Funcrionou");
+    }
+})
+
 app.listen(5678, (req,res) => {
     console.log("Servidor rodando");
 })
